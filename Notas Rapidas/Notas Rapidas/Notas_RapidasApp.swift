49bb482542bc5b7129fef2550a8481aea69fb413
Miguel Jonathan Mogrovejo Cardenas <52843077+mjmc441998@users.noreply.github.com>
2021-05-04 23:52:43 -0500

    Add files via upload

---
+++ Notas Rapidas/Notas Rapidas/Notas_RapidasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notas_RapidasApp.swift
+//  Notas Rapidas
+//
+//  Created by MJMC on 4/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Notas_RapidasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
